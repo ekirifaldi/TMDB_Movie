12a055bcaf7a453f36fbb05a137098824a5aedbc
ekirifaldi <ekirifaldi2010@gmail.com>
2021-01-10 19:54:09 +0700

    Inisiate Project

---
+++ TMDBMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TMDBMovie
+//
+//  Created by Eki Rifaldi on 10/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
